5c4ec669e8eff402ef4cc1c0409726df0fa69997
simonofyork <134941680+simonofyork@users.noreply.github.com>
2023-05-29 14:18:28 +0100

    Update streamlit_app.py
    Added text formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-streamlit.title('Hard-Boiled FreeRange Egg')
+streamlit.title2('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled FreeRange Egg')
